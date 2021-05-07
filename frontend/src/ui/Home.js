b985137dd83d977dd735bbc2a4983021cdd18fb8
Samuel Martinez <semartinez147@gmail.com>
2021-05-07 13:42:10 -0600

    Completed layout

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -8,6 +8,10 @@ import Nav from "react-bootstrap/Nav";
 import {Card, CardDeck, Col, Row} from "react-bootstrap";
 import murray from "../murray.jpg";
 import card1 from "../card1.jpg";
+import card2 from "../card2.jpg";
+import card3 from "../card3.jpg";
+import card4 from "../card4.jpg";
+import squiggle from "../squiggle.png";
 
 export function Home() {
     return (
@@ -34,60 +38,113 @@ export function Home() {
                     </p>
                 </Jumbotron>
             </Container>
-            <Container fluid className="bg-secondary py-5">
+            <Container fluid className="bg-secondary py-4 my-2">
                 <Row className="align-content-center justify-content-between">
                     <Col md={4} lg={3} className="mx-3 my-md-auto text-center">
                         <Image src={murray}/>
                     </Col>
                     <Col md={7} lg={8} className="mx-3 mt-3 align-self-center">
-                        <p>It was a soft, reposeful summer landscape, as lovely as a dream, and as lonesome as Sunday.  The air was full of the smell of flowers, and the buzzing of insects, and the twittering of birds, and there were no people, no wagons, there was no stir of life, nothing going on.  The road was mainly a winding path with hoof-prints in it, and now and then a faint trace of wheels on either side in the grass—wheels that apparently had a tire as broad as one’s hand.</p>
-                        <p>Presently a fair slip of a girl, about ten years old, with a cataract of golden hair streaming down over her shoulders, came along. Around her head she wore a hoop of flame-red poppies. It was as sweet an outfit as ever I saw, what there was of it.  She walked indolently along, with a mind at rest, its peace reflected in her innocent face.  The circus man paid no attention to her; didn’t even seem to see her.  And she—she was no more startled at his fantastic make-up than if she was used to his like every day of her life.  She was going by as indifferently as she might have gone by a couple of cows; but when she happened to notice me, then there was a change!</p>
+                        <p>It was a soft, reposeful summer landscape, as lovely as a dream, and as lonesome as Sunday.
+                            The air was full of the smell of flowers, and the buzzing of insects, and the twittering of
+                            birds, and there were no people, no wagons, there was no stir of life, nothing going on. The
+                            road was mainly a winding path with hoof-prints in it, and now and then a faint trace of
+                            wheels on either side in the grass—wheels that apparently had a tire as broad as one’s
+                            hand.</p>
+                        <p>Presently a fair slip of a girl, about ten years old, with a cataract of golden hair
+                            streaming down over her shoulders, came along. Around her head she wore a hoop of flame-red
+                            poppies. It was as sweet an outfit as ever I saw, what there was of it. She walked
+                            indolently along, with a mind at rest, its peace reflected in her innocent face. The circus
+                            man paid no attention to her; didn’t even seem to see her. And she—she was no more startled
+                            at his fantastic make-up than if she was used to his like every day of her life. She was
+                            going by as indifferently as she might have gone by a couple of cows; but when she happened
+                            to notice me, then there was a change!</p>
                     </Col>
                 </Row>
             </Container>
-            <Container fluid>
+
+            <Container fluid className="my-4">
                 <CardDeck>
-                    <Card>
-                        <Card.Img variant="top" src="holder.js/100px160" />
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This content is a little bit longer.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="top" src={card1} />
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This card has supporting text below as a natural lead-in to additional
-                                content.{' '}
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card>
-                    <Card>
-                        <Card.Img variant="top" src="holder.js/100px160" />
-                        <Card.Body>
-                            <Card.Title>Card title</Card.Title>
-                            <Card.Text>
-                                This is a wider card with supporting text below as a natural lead-in to
-                                additional content. This card has even longer content than the first to
-                                show that equal height action.
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">Last updated 3 mins ago</small>
-                        </Card.Footer>
-                    </Card>
+                    <Row>
+                        <Col sm={6} lg={3}>
+                            <Card border="light" className="mx-4">
+                                <Card.Img variant="top" src={card1} roundedCircle/>
+                                <Card.Body>
+                                    <Card.Text>
+                                        Beyond the spaceport gates, the men of the Kharsa were hunting down a thief. I
+                                        heard
+                                        the
+                                        shrill cries, the pad-padding of feet in strides just a little too long and
+                                        loping
+                                        to be
+                                        human, raising echoes all down the dark and dusty streets leading up to the main
+                                        square.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card></Col>
+                        <Col sm={6} lg={3}>
+                            <Card border="light" className="mx-4">
+                                <Card.Img variant="top" src={card2}/>
+                                <Card.Body>
+                                    <Card.Text>
+                                        But the square itself lay empty in the crimson noon of Wolf. Overhead the dim
+                                        red
+                                        ember
+                                        of Phi Coronis, Wolf's old and dying sun, gave out a pale and heatless light.
+                                        The
+                                        pair
+                                        of Spaceforce guards at the gates, wearing the black leathers of the Terran
+                                        Empire,
+                                        shockers holstered at their belts, were drowsing under the arched gateway where
+                                        the
+                                        star-and-rocket emblem proclaimed the domain of Terra.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card></Col>
+                        <Col sm={6} lg={3}>
+                            <Card border="light" className="mx-4">
+                                <Card.Img variant="top" src={card3}/>
+                                <Card.Body>
+                                    <Card.Text>
+                                        One of them, a snub-nosed youngster only a few weeks out from Earth, cocked an
+                                        inquisitive ear at the cries and scuffling feet, then jerked his head at
+                                        me.<br/>
+                                        "Hey, Cargill, you can talk their lingo. What's going on out there?"
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col sm={6} lg={3}>
+                            <Card border="light" className="mx-4">
+                                <Card.Img variant="top" src={card4}/>
+                                <Card.Body>
+                                    <Card.Text>
+                                        I stepped out past the gateway to listen. There was still no one to be seen in
+                                        the
+                                        square. It lay white and windswept, a barricade of emptiness; to one side the
+                                        spaceport
+                                        and the white skyscraper of the Terran Headquarters, and at the other side, the
+                                        clutter
+                                        of low buildings, the street-shrine, the little spaceport cafe smelling of
+                                        coffee
+                                        and
+                                        jaco, and the dark opening mouths of streets that rambled down into the
+                                        Kharsa—the
+                                        old
+                                        town, the native quarter.
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
                 </CardDeck>
+            </Container>
+
+            <Container fluid>
+                <Row className="bg-dark text-center">
+                    <Col>
+                        <Image src={squiggle}/>
+                    </Col>
+                </Row>
             </Container>
         </>
     )
