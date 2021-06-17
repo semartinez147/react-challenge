06005ad688f31adbe2f44b2e26181054121fadbf
Samuel Martinez <semartinez147@gmail.com>
2021-06-17 14:05:17 -0600

    testing BrowserRouter

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -1,19 +1,19 @@
 import React from "react"
 import 'bootstrap/dist/css/bootstrap.css';
 import "./App.css"
-import {Route, BrowserRouter, Switch, HashRouter} from 'react-router-dom'
+import {Route, BrowserRouter, Switch} from 'react-router-dom'
 import {Home} from './Home'
 import {About} from "./About";
 
 export function App() {
 	return(
 		<>
-			<HashRouter >
+			<BrowserRouter basename={'/react-challenge'}>
 				<Switch>
-					<Route exact path='/#/' component={Home} />
+					<Route exact path='/' component={Home} />
 					<Route exact path='/about' component={About} />
 				</Switch>
-			</HashRouter>
+			</BrowserRouter>
 		</>
 	)
 
